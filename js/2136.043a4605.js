aa8cc64760cc11091f6c275b99e90b6606ff6acc
ashish <asespandey2@gmail.com>
2022-12-04 18:45:29 +0545

    first commit

---
+++ js/2136.043a4605.js
@@ -0,0 +1,1 @@
+"use strict";(self["webpackChunktailkit"]=self["webpackChunktailkit"]||[]).push([[2136],{12136:function(e,t,n){n.r(t),n.d(t,{default:function(){return s}});var r=n(1987);function u(e,t){const n=(0,r.up)("base-page-preview");return(0,r.wg)(),(0,r.j4)(n)}var a=n(40089);const i={},c=(0,a.Z)(i,[["render",u]]);var s=c}}]);
